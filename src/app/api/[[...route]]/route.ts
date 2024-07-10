85399a787784d64ff94131dfda42f40ed5056ad9
Vinoth <vinoth.kumar@credo.health>
2024-07-10 10:03:11 +0530

    hono implemented

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,8 +3,6 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { handle } from "hono/vercel";
 
-export const runtime = "edge";
-
 const app = new Hono().basePath("/api");
 
 app.use(logger());
